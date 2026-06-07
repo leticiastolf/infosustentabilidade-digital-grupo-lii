abad3d5beda2c12691f9b3fa22ee1673893e6fb0
le <leticia.stolf@unidavi.edu.br>
2026-06-07 14:18:06 -0300

    Update script.sql

--- sql/script.sql
+++ sql/script.sql
@@ -1,1 +1,14 @@
-![Evidência da consulta SQL](evidencias/consulta-sql.png)
+## Script consulta SQL no supabase:
+
+SELECT
+    pc.nome_local,
+    pc.cidade,
+    ma.tipo_material,
+    pd.modalidade,
+    pd.possui_custo
+FROM pontos_coleta pc
+INNER JOIN materiais_aceitos ma
+    ON pc.id_ponto = ma.id_ponto
+INNER JOIN procedimentos_descarte pd
+    ON pc.id_ponto = pd.id_ponto
+ORDER BY pc.nome_local;
